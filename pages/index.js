73bc3b0acdcf39d00715176e80a208ab853acfba
Marvin Roellecke <marvin@roellecke.com>
2022-12-01 10:24:25 +0100

    create header component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
+import Header from "../components/Header/Header";
+
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
-  );
+  return <Header />;
 }
